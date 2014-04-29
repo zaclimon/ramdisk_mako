b3a8a774dee0a9f7d983b66314648406d03e62e8
franciscofranco <franciscofranco.1990@gmail.com>
2014-04-29 16:26:09 +0100

    Make sure root works regardless of the binary used (supersu/superuser).
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- boot.img-ramdisk/sbin/fkbootscript.sh
+++ boot.img-ramdisk/sbin/fkbootscript.sh
@@ -1,17 +1,27 @@
 #!/system/bin/sh
 
+# custom busybox installation shortcut
+bb=/sbin/bb/busybox;
+
+# ensure SuperSU daemonsu/Superuser su_daemon service is running
+$bb [ -e /system/xbin/daemonsu ] && /system/xbin/daemonsu --auto-daemon &
+$bb [ ! -e /system/xbin/daemonsu ] &&  /system/xbin/su --daemon &
+
 # disable sysctl.conf to prevent ROM interference with tunables
-# backup and replace PowerHAL with custom build to allow OC/UC to survive screen off
+$bb mount -o rw,remount /system;
+$bb [ -e /system/etc/sysctl.conf ] && $bb mv -f /system/etc/sysctl.conf /system/etc/sysctl.conf.fkbak;
+
+# disable the PowerHAL since there is a kernel-side touch boost implemented
+$bb [ -e /system/lib/hw/power.msm8960.so.fkbak ] || $bb cp /system/lib/hw/power.msm8960.so /system/lib/hw/power.msm8960.so.fkbak;
+$bb [ -e /system/lib/hw/power.msm8960.so ] && $bb rm -f /system/lib/hw/power.msm8960.so;
+
 # create and set permissions for /system/etc/init.d if it doesn't already exist
-mount -o rw,remount /system /system;
-[ -e /system/etc/sysctl.conf ] && mv /system/etc/sysctl.conf /system/etc/sysctl.conf.fkbak;
-[ -f /system/lib/hw/power.msm8960.so.bak ] || mv /system/lib/hw/power.msm8960.so /system/lib/hw/power.msm8960.so.bak
 if [ ! -e /system/etc/init.d ]; then
-  mkdir /system/etc/init.d
-  chown -R root.root /system/etc/init.d;
-  chmod -R 755 /system/etc/init.d;
+  $bb mkdir /system/etc/init.d;
+  $bb chown -R root.root /system/etc/init.d;
+  $bb chmod -R 755 /system/etc/init.d;
 fi;
-mount -o ro,remount /system /system;
+$bb mount -o ro,remount /system;
 
 echo 20000 1300000:40000 1400000:20000 > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
 echo 85 1300000:90 1400000:70 > /sys/devices/system/cpu/cpufreq/interactive/target_loads
